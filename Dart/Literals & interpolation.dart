c3bc7c32c71a761ffbff43bc8c36d2e4b7b39267
Obaydullah Khan <obaydullahkhaan@gmail.com>
2023-08-22 01:58:58 +0600

    Literals & Interpolation

---
+++ Dart/Literals & interpolation.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+
+
+  String name = "Obaydullah";
+  int age = 23;
+  double cgpa = 3.2;
+  69;
+  "mirpur";
+
+  print("my name is $name , it's a long name of ${name.length}. $age + $cgpa = ${age+cgpa}");
+
+
+}
