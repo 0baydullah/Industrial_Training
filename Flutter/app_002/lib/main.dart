37dbc162133ee46d47a9ba061ae3752d9ab18be7
Obaydullah Khan <obaydullahkhaan@gmail.com>
2023-09-06 13:07:16 +0600

    structure

--- Flutter/app_002/lib/main.dart
+++ Flutter/app_002/lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:app_002/pages/splashScreen.dart';
+import 'package:app_002/pages/login.dart';
+import 'package:app_002/pages/signup.dart';
+import 'package:app_002/pages/home.dart';
+import 'package:app_002/pages/home2.dart';
 
 void main() {
   runApp(
+}
 
-  );
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => Login(),
+        '/home': (context) => Home(),
+        '/signup': (context) => Signup(),
+      },
+    );
+  }
 }
